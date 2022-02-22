880241b4dc6fd93a640a30ccd1512ead400fe22b
SvenJuergens <SvenJuergens@users.noreply.github.com>
2022-02-22 09:24:59 +0100

    [TASK] Version 0.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
             'typo3' => '10.4.0-10.4.99',
         ],
     ],
-    'version' => '0.1.0',
+    'version' => '0.5.0',
     'autoload' => [
         'psr-4' => [
             'Svenjuergens\\Minidownload\\' => 'Classes/',
